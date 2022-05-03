086c565b3ef0cbb94db52d67d2956272f6e2626b
wendel4891 <wendel4891@gmail.com>
2022-05-02 22:56:24 -0300

    add método get_absolute_url no modelo Post.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -32,6 +33,17 @@ class Post(models.Model):
                     default='draft')
     objects = models.Manager() # o gerenciador default
     published = PublishedManager() # o gerenciador personalizado
+    
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
     
     class Meta:
         ordering = ('-publish',)
